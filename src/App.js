de6834fe5820f9fc7b396bb1b24c5ca793d5b391
Raidə Məmmədova <raida.mammadova@nspsolutions.net>
2020-02-25 08:07:16 +0400

    signInAndSignUp component created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Route, Switch} from "react-router-dom";
+
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
 import './App.css';
 
 function App() {
@@ -12,6 +15,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={HomePage}/>
                 <Route exact path="/shop" component={ShopPage}/>
+                <Route exact path="/signin" component={SignInAndSignUpPage}/>
             </Switch>
         </div>
     );
