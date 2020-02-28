8b2968727b278e325dfa40c3a9744e27f0e1208b
Raidə Məmmədova <raida.mammadova@nspsolutions.net>
2020-02-28 09:40:02 +0400

    tiny ches

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -43,14 +43,12 @@ export const createUserProfileDocument = async (userAuth, additionalData)=>{
 
 firebase.initializeApp(config);
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
-
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({
     prompt: "select_account",
 });
 
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
 export default firebase;
