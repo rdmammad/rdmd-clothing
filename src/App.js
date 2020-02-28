918682abc11a56633cc5d6adb7cc331778e08fac
Raidə Məmmədova <raida.mammadova@nspsolutions.net>
2020-02-28 08:32:47 +0400

    storing users to firestore added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils.js";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils.js";
 
 import './App.css';
 
@@ -21,9 +21,21 @@ class App extends React.Component{
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user});
-            console.log(user)
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+                if(userAuth){
+                    const userRef = await createUserProfileDocument(userAuth);
+
+                    userRef.onSnapshot(snapshot => {
+                        this.setState({
+                            currentUser: {
+                                id: snapshot.id,
+                                ...snapshot.data(),
+                            },
+                        });
+                    });
+                }
+                else
+                    this.setState({currentUser: userAuth});
         })
     }
 
