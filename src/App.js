39ce0a0cad96cbc0369ce82ddb490b95d73d2de3
Раида Мамедова <raidamamedova96@gmail.com>
2020-03-07 12:42:07 +0400

    user selector added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Redirect, Route, Switch} from "react-router-dom";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils.js";
+import {connect} from "react-redux";
+import {createStructuredSelector} from "reselect";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth, createUserProfileDocument} from "./firebase/firebase.utils.js";
 import NotFoundPage from "./components/not-found-page/not-found-page.component"
-import {connect} from "react-redux";
 import {setCurrentUser} from "./redux/user/user.actions";
+import {selectCurrentUser} from "./redux/user/user.selectors";
 
 import './App.css';
 
@@ -53,8 +55,8 @@ class App extends React.Component {
     }
 }
 
-const mapStateToProps = ({user}) => ({
-    currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
