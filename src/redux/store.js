80f378f74e1c3f2083066012d247908a9d2ac74c
Раида Мамедова <raidamamedova96@gmail.com>
2020-03-08 11:40:53 +0400

    pedux-persist added

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,9 @@ import {createStore, applyMiddleware} from "redux";
 // this is nice for us to use when debugging redux code
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
+import {persistStore} from "redux-persist";
 
 
 const middlewares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-export default store;
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
