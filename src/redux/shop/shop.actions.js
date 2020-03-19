2c362a06142798914b21e74afe5ef7c69d4990b1
Раида Мамедова <raidamamedova96@gmail.com>
2020-03-19 17:26:02 +0400

    redux thunk implemented for asyng functionality

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,31 @@
 import {ShopActionsTypes} from "./shop.types";
+import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
 
-export const updateCollections = collectionsMap => ({
-    type: ShopActionsTypes.UPDATE_COLLECTION,
+export const fetchCollectionsStart = () => ({
+    type: ShopActionsTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionsTypes.FETCH_COLLECTIONS_SUCCESS,
     payload: collectionsMap,
 });
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionsTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage,
+});
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionsStart());
+
+        collectionRef
+            .get()
+            .then(async snapshot => {
+                const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+                dispatch(fetchCollectionsSuccess(collectionsMap));
+            })
+            .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+    };
+};
