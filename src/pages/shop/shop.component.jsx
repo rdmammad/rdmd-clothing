5abc83d06611611ed2ef2629ad85fd1c1c3c5a00
Раида Мамедова <raidamamedova96@gmail.com>
2020-03-19 16:29:25 +0400

    get collections from firestore using get(), which returns a Promise

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -14,17 +14,18 @@ class ShopPage extends React.Component {
     state = {
         loading: true,
     };
-    unsubscribeFromSnapshot = null;
 
     componentDidMount() {
         const {updateCollections} = this.props;
         const collectionRef = firestore.collection('collections');
 
-        collectionRef.onSnapshot(async snapshot => {
+        collectionRef
+            .get()
+            .then(async snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
             this.setState({loading: false});
-        })
+        });
     }
 
     render() {
