179d9b3a091e89fa025a6774b205ef8e6419a8e8
Раида Мамедова <raidamamedova96@gmail.com>
2020-06-16 19:16:57 +0400

    changed ShopPage classfull component to functional component w/ using useEffect hooks

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,33 +1,28 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Route} from "react-router-dom";
 import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
 import CollectionPageContainer from "../collection/collection.container";
 import {connect} from "react-redux";
 import {fetchCollectionsStartAsync} from "../../redux/shop/shop.actions";
 
-class ShopPage extends React.Component {
-    componentDidMount() {
-        const {fetchCollectionsStartAsync} = this.props;
+const ShopPage = ({match, fetchCollectionsStartAsync}) => {
+    useEffect(() => {
         fetchCollectionsStartAsync();
-    }
+    }, [fetchCollectionsStartAsync]);
 
-    render() {
-        const {match} = this.props;
-
-        return (
-            <div className='shop-page'>
-                <Route
-                    exact path={`${match.path}`}
-                    component={CollectionsOverviewContainer}
-                />
-                <Route
-                    path={`${match.path}/:collectionId`}
-                    component={CollectionPageContainer}
-                />
-            </div>
-        );
-    }
-}
+    return (
+        <div className='shop-page'>
+            <Route
+                exact path={`${match.path}`}
+                component={CollectionsOverviewContainer}
+            />
+            <Route
+                path={`${match.path}/:collectionId`}
+                component={CollectionPageContainer}
+            />
+        </div>
+    );
+};
 
 const mapDispatchToProps = dispatch => ({
     fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
