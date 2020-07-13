93b1be4aad7ff6ac4d6ad222b3480db93fd39fe4
Раида Мамедова <raidamamedova96@gmail.com>
2020-07-13 11:31:03 +0400

    converting to PWA

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import {ApolloProvider} from "react-apollo";
 import {createHttpLink} from "apollo-link-http";
 import {InMemoryCache} from "apollo-cache-inmemory";
 import {ApolloClient, gql} from 'apollo-boost';
+import * as serviceWorker from "./serviceWorker";
 
 import './index.css';
 import App from './App';
@@ -34,3 +35,5 @@ ReactDOM.render(
         </Provider>
     </ApolloProvider>
     , document.getElementById('root'));
+
+serviceWorker.register();
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require("body-parser");
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production')
     require('dotenv').config();
@@ -18,6 +19,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 if(process.env.ENV === 'production'){
+    // redirect to HTTPS when HTTP entered
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', (req, res) => {
@@ -28,6 +32,10 @@ if(process.env.ENV === 'production'){
 app.listen(port, error => {
     if(error) throw error;
     console.log(`Server is running on port ${port}`)
+});
+
+app.get('/service-worker.js', (req, res) => {
+    res.send(path.resolve(__dirname, '..', 'build', 'service-worker.js'))
 });
 
 app.post('/payment', (req, res) => {
